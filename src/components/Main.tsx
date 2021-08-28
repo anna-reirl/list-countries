cdaf48d4e6270b9f0ce1efd3ee46ffcca41f7e79
Anna <anna_agabekyan_anna@mail.ru>
2021-08-28 16:45:27 +0400

    feat: transferring data between components

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import Countries from './Countries'
+import Alerts from './Alerts'
 import '../styles/Main.css'
 
 
 function Main() {
+    let alertText = "This is some text for alert"
         return (
             <main>
+                <Alerts>{alertText}</Alerts>
                 <Countries />
             </main>
         )
