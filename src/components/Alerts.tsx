c80da766b5783aa07a71bdc4a975a385d69e5109
Anna <anna_agabekyan_anna@mail.ru>
2021-08-29 16:58:16 +0400

    feat: iteration of the content of the alert component

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -2,15 +2,13 @@ import * as React from 'react'
 import Alert from 'react-bootstrap/Alert'
 
 function Alerts(props) {
+    let children = props.children
     return (
         <>
-        {
-            <Alert variant={"danger"}>
-              {props.children}
-            </Alert>
-        }
+            {
+               React.Children.count(props.children)
+            } 
         </>
-        
     )
 }
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,14 +2,24 @@ import * as React from 'react'
 import Countries from './Countries'
 import Alerts from './Alerts'
 import '../styles/Main.css'
-
+import Alert from 'react-bootstrap/Alert'
 
 function Main() {
     let alertText = "This is some text for alert"
         return (
             <main>
-                <Alerts>{alertText}</Alerts>
-                <Countries />
+            <Alerts>
+                <Alert variant={"danger"}>
+                {alertText}
+                </Alert>
+                <Alert variant={"success"}>
+                {alertText}
+                </Alert>
+                <Alert variant={"warning"}>
+                {alertText}
+                </Alert>
+            </Alerts>
+            <Countries />
             </main>
         )
     }
